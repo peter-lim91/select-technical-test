047319a15f9ac8b22ce2b1da1d2afb4630560425
peter-lim91 <peter.lim91@gmail.com>
2023-08-07 15:10:17 +1200

    feat: add menu placeholder

--- src/app/components/FaceBox.tsx
+++ src/app/components/FaceBox.tsx
@@ -1,24 +1,45 @@
 import { Face } from "@/types/Face"
+import { useState } from "react"
 
 const formatPercentage = (percentage: number) => {
   return `${percentage * 100}%`
 }
 
 export const FaceBox = ({ id, xmax, xmin, ymax, ymin }: Face) => {
+  const [showOptions, setShowOptions] = useState(false)
   const handleClick = () => {
-    console.log(id)
+    setShowOptions((b) => !b)
   }
+
   return (
     <div
       style={{
         position: "absolute",
         top: formatPercentage(ymin),
         left: formatPercentage(xmin),
-        width: formatPercentage(xmax - xmin),
-        height: formatPercentage(ymax - ymin),
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "row",
       }}
-      className="border-2 border-white"
-      onClick={handleClick}
-    ></div>
+    >
+      <div
+        style={{
+          width: formatPercentage(xmax - xmin),
+          height: formatPercentage(ymax - ymin),
+        }}
+        className="border-2 border-white"
+        onClick={handleClick}
+      />
+      {showOptions && (
+        <>
+          <div>
+            <div>Dummy Item #1</div>
+            <div>Dummy Item #2</div>
+            <div>Dummy Item #2</div>
+          </div>
+        </>
+      )}
+    </div>
   )
 }
--- src/app/components/SelectImageCarousel.tsx
+++ src/app/components/SelectImageCarousel.tsx
@@ -32,12 +32,15 @@ export const SelectImageCarousel = () => {
         <SelectImageSingle id={id} filename={filename} url={url} />
         <div className="absolute bottom-0 left-0 w-full">
           <div className="flex flex-row justify-between p-2 m-2 bg-white text-black">
-            <button onClick={handleClickPrevious}>
-              <ArrowLeft />
+            <button onClick={handleClickPrevious} className="hover:bg-gray-500">
+              <ArrowLeft className="w-6 h-6" />
             </button>
-            <div>{filename}</div>
+            <p>{filename}</p>
             <button onClick={handleClickNext}>
-              <ArrowRight />
+              <ArrowRight
+                className="w-6 h-6
+              "
+              />
             </button>
           </div>
         </div>
